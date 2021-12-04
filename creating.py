a6750b59d163b3365b6ee498f6da9ce07a3f31ee
MrComputer8398 <70330051+MrComputer8398@users.noreply.github.com>
2021-12-04 19:17:32 +0100

    Create creating.py

---
+++ creating.py
@@ -0,0 +1,17 @@
+import time
+
+def wait(x):
+  time.sleep(x)
+
+print("______")
+
+for i in range(6):
+  print("|")
+  
+for i in range(6):
+  print("      |")
+  
+wait(1)
+
+print("______")
+               
